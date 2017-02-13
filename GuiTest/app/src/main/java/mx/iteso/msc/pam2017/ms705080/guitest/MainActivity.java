6e82d2070ae0082d75e547ec522e87868142f648
Mario Contreras <marioc@nazul.net>
2017-02-13 16:57:49 -0600

    GuiTest Project - Homework completed

--- GuiTest/app/src/main/java/mx/iteso/msc/pam2017/ms705080/guitest/MainActivity.java
+++ GuiTest/app/src/main/java/mx/iteso/msc/pam2017/ms705080/guitest/MainActivity.java
@@ -17,23 +17,31 @@
  */
 package mx.iteso.msc.pam2017.ms705080.guitest;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Spinner -> Scholarship
-        Spinner scholarship = (Spinner)findViewById(R.id.spinScholarship);
+        Spinner scholarship = (Spinner) findViewById(R.id.spinScholarship);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.scholarship, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
@@ -43,9 +51,58 @@ public class MainActivity extends AppCompatActivity {
 
         // AutoCompleteTextView -> Favorite Book
         adapter = ArrayAdapter.createFromResource(this, R.array.books, android.R.layout.select_dialog_item);
-        AutoCompleteTextView fb = (AutoCompleteTextView)findViewById(R.id.actvFavBook);
+        AutoCompleteTextView fb = (AutoCompleteTextView) findViewById(R.id.actvFavBook);
         fb.setThreshold(1);
         fb.setAdapter(adapter);
+
+        CheckedTextView ctvSports = (CheckedTextView) findViewById(R.id.ctvSportsPractice);
+        ctvSports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckedTextView sports = (CheckedTextView) v;
+                sports.setChecked(!sports.isChecked());
+            }
+        });
+
+        Button clearButton = (Button) findViewById(R.id.btnClear);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("")
+                        .setMessage(getString(R.string.textConfirmClear))
+                        .setPositiveButton(R.string.textClear, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                clearFields();
+                            }
+                        })
+                        .setNegativeButton(R.string.textCancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
+    }
+
+    private void clearFields() {
+        EditText etName = (EditText) findViewById(R.id.editName);
+        EditText etPhone = (EditText) findViewById(R.id.editPhone);
+        Spinner spScholarship = (Spinner) findViewById(R.id.spinScholarship);
+        RadioButton rbMale = (RadioButton) findViewById(R.id.rbMale);
+        AutoCompleteTextView actvFavBook = (AutoCompleteTextView) findViewById(R.id.actvFavBook);
+        CheckedTextView ctvSports = (CheckedTextView) findViewById(R.id.ctvSportsPractice);
+
+        etName.setText("");
+        etPhone.setText("");
+        spScholarship.setSelection(0);
+        rbMale.setChecked(true);
+        actvFavBook.setText("");
+        ctvSports.setChecked(false);
+
+        Toast.makeText(getApplicationContext(), getString(R.string.textAllFieldsCleared), Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -53,6 +110,31 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_save:
+                EditText etName = (EditText) findViewById(R.id.editName);
+                EditText etPhone = (EditText) findViewById(R.id.editPhone);
+                Spinner spScholarship = (Spinner) findViewById(R.id.spinScholarship);
+                RadioButton rbMale = (RadioButton) findViewById(R.id.rbMale);
+                AutoCompleteTextView actvFavBook = (AutoCompleteTextView) findViewById(R.id.actvFavBook);
+                CheckedTextView ctvSports = (CheckedTextView) findViewById(R.id.ctvSportsPractice);
+                String name = etName.getText().toString();
+                String phone = etPhone.getText().toString();
+                String scholarship = spScholarship.getSelectedItem().toString();
+                String gender = rbMale.isChecked() ? getString(R.string.textMale) : getString(R.string.textFemale);
+                String favBook = actvFavBook.getEditableText().toString().isEmpty() ? getString(R.string.textNoFavBook) : actvFavBook.getEditableText().toString();
+                String sports = ctvSports.isChecked() ? getString(R.string.textYes) : getString(R.string.textNo);
+
+                String msg = java.text.MessageFormat.format(getString(R.string.textDataSaved), "\n", name, phone, scholarship, gender, favBook, sports);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
 
